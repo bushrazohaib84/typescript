95f8810d17c6527dbe3f7df375902b1856a28688
bushrazohaib84 <154512354+bushrazohaib84@users.noreply.github.com>
2024-01-05 21:43:48 +0500

    Add files via upload

---
+++ PromptCalculator/main.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prompt = require('prompt-sync')();
+let n1 = parseInt(prompt(`please enter your 1st number:`));
+let n2 = parseInt(prompt(`please enter your 2nd number:`));
+let operation = prompt("write any 1 operation from the following:(+,-,*,/):");
+if (operation == "+") {
+    console.log(`your answer is ${n1} ${operation} ${n2} = ${n1 + n2}`);
+}
+else if (operation == "-") {
+    console.log(`your answer is ${n1} ${operation} ${n2} = ${n1 - n2}`);
+}
+else if (operation == "*") {
+    console.log(`your answer is ${n1} ${operation} ${n2} = ${n1 * n2}`);
+}
+else if (operation == "/") {
+    console.log(`your answer is ${n1} ${operation} ${n2} = ${n1 / n2}`);
+}
+else {
+    console.log("choose correct answer");
+}
