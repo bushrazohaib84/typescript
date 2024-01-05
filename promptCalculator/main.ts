f1516c100fb28592a4aedcbe2afde0af6cfb964b
bushrazohaib84 <154512354+bushrazohaib84@users.noreply.github.com>
2024-01-05 21:34:48 +0500

    Add files via upload

--- promptCalculator/main.ts
+++ promptCalculator/main.ts
@@ -1,1 +1,10 @@
+import inquirer from "inquirer";
 
+async function main(){
+    const answer = await inquirer.prompt([{
+        type:"number",
+        name:"n1",
+        message:"Enter First Number"
+    }])
+    console.log(answer)
+}
