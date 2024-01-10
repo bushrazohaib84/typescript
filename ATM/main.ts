246e0cd9452166f07c2d0d9b3077697f1ccbe6e9
Bushra Zohaib <154512354+bushrazohaib84@users.noreply.github.com>
2024-01-10 22:38:01 +0500

    Add files via upload

--- ATM/main.ts
+++ ATM/main.ts
@@ -1,1 +1,74 @@
+import inquirer from 'inquirer';
+import { faker } from '@faker-js/faker';
+interface user {
+    id:number
+    pin:number
+    name:string
+    accountNumber:number
+    balance:number
+}
+const createUser = ()=>{
+let users:user[]=[] 
 
+for(let i=0; i<5;i++){
+    let user:user = {
+        id:i,
+        pin:1000 +i,
+        name:faker.person.fullName(),
+        accountNumber:Math.floor(1000000000 * Math.random()*900000000),
+        balance:1000000 * i 
+    }
+    users.push(user)
+}
+//  console.log(users)
+return users
+};
+// ATM
+const atmMachine  = async(users:user [])=>{
+    const res = await inquirer.prompt({
+        type:"number",
+        message: "enter pin code",
+        name:"pin",
+    });
+    
+    console.log(res)
+    const user = users.find((val) => val.pin == res.pin);
+    if(user){
+        console.log(`welcome ${user.name}`);
+        atmFunc(user)
+        return;
+    }else{
+    console.log("invalid user pin");}
+};
+
+// Atm Function
+const atmFunc = async(user:user)=>{
+const ans = await inquirer.prompt({
+    type:"list",
+    name:"select",
+    choices:["withdraw", "balance", "exit"],
+    message:"desired action"
+})
+
+if(ans.select == "withdraw"){
+    const amount = await inquirer.prompt({
+        type:"number",
+        name:"rupee",
+        message:"enter amount",
+    })
+if (amount.rupee > user.balance){
+    return console.log("insuffitient balance")
+}
+console.log(`withdraw amount: ${amount.rupee}`)
+console.log(`Balance: ${user.balance-amount.rupee}`)
+}
+if (ans.select== "balance"){
+    console.log(`Balance: ${user.balance}`)
+    return
+} 
+if (ans.select== "exit"){
+    console.log("thanks for using Atm")
+console.log(ans)
+}
+const users = createUser()
+atmMachine(users)}
